b6c7d3776df7c3469a3f977834e3dfd06d34d3f1
marianagrana2 <mariana.grana2@gmail.com>
2024-05-09 16:57:29 -0600

    Updated Java Project

---
+++ src/main/java/com/mycompany/peluqueriacaninaproyecto/PeluqueriaCaninaProyecto.java
@@ -0,0 +1,12 @@
+package com.mycompany.peluqueriacaninaproyecto;
+
+import com.mycompany.peluqueriacaninaproyecto.igu.Principal;
+
+public class PeluqueriaCaninaProyecto {
+
+    public static void main(String[] args) {
+       Principal pantalla = new Principal();
+       pantalla.setVisible(true);
+       pantalla.setLocationRelativeTo(null);
+    }
+}
